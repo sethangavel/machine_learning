ab10a882dce4b270bdf5043f51d4c4f204987b83
sethangavel <senthil.thangavel@hotmail.com>
2017-11-11 12:09:45 -0800

    Initial version

---
+++ IrisClass/IrisClass.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+###########################################################################################
+# ScriptName :  
+# Author : sethangavel
+# Purpose : 
+# Date Created : Sat Nov 11 12:01:33 2017
+###########################################################################################
+
+#### Load Libraries
+import pandas
+from pandas.tools.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+
+
